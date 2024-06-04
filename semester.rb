627d7c67cba1249ddb5930a60a9c10ad07c6573c
André Calero Valdez <andrecalerovaldez@gmail.com>
2024-06-04 08:57:48 +0200

    updated my plan and semester.rb

---
+++ semester.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require_relative './shared'
+FILE_NAME = "semester.pdf"
+
+puts "Generating a semester page into #{FILE_NAME}"
+
+options = { locale: 'en' }
+OptionParser.new do |parser|
+parser.banner = "Usage: #{$PROGRAM_NAME} [options]"
+parser.on('-l', '--locale LOCALE', 'Locale to use for internationalization')
+parser.on("-h", "--help", "Prints this help") do
+  puts parser
+  exit
+end
+end.parse!(into: options)
+
+init_i18n(options[:locale])
+
+pdf = init_pdf
+hole_punches pdf
+
+heading_left = I18n.t('semester_overview')
+notes_page pdf, heading_left
+begin_new_page pdf, :left
+notes_page pdf, heading_left
+
+pdf.render_file FILE_NAME
